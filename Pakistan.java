a4a90db322a62887fdf0ced22928ecdca67c2b9f
abdullahqureshi1609 <abdullahaftab97@gmail.com>
2024-04-26 00:36:47 +0500

    java class added

---
+++ Pakistan.java
@@ -0,0 +1,17 @@
+public class Pakistan{
+    static String city;
+
+    public static String getCity() {
+        return city;
+    }
+
+    public static void setCity(String city) {
+        Pakistan.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Pakistan []";
+    }
+    
+}
